fee92b3d54eddd9d1bfee19995b4aa26efeeaffb
jessecravens <jesse.cravens@gmail.com>
2014-05-08 09:03:57 -0500

    Chapter 8: Generate the application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RocknrollcallRails::Application.config.secret_key_base = 'f1f86f53984ad559fd4728ed863a935fa5d5f05a1275eb0de77a7945eff84789828eca28dc6778a8b0dc84d5b2f215f9c7f634053a252d62e1021e70546c45df'
