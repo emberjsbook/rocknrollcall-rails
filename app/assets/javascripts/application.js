a4a248aa93faa1e948a66ff11becc18b8ec58887
jessecravens <jesse.cravens@gmail.com>
2014-05-08 09:28:32 -0500

    Chapter 8: Generate the Ember application.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require rocknrollcall_rails
+
+// for more details see: http://emberjs.com/guides/application/
+RocknrollcallRails = Ember.Application.create();
+
 //= require_tree .
---
+++ app/assets/javascripts/router.js
@@ -0,0 +1,5 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+RocknrollcallRails.Router.map(function() {
+  // this.resource('posts');
+});
