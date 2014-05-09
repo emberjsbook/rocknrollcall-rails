25f2e68f303860722d2dba63c41a5c912504c456
jessecravens <jesse.cravens@gmail.com>
2014-05-08 23:39:22 -0500

    Chapter 8: Update the Router with the activity resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RocknrollcallRails::Application.routes.draw do
-  get "activities/index"
+  resources :activities
   get "home/index"
   root :to => 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
