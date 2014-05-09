e98256a8a421dc9d36c51d490e53cd394bbbdb15
jessecravens <jesse.cravens@gmail.com>
2014-05-08 23:38:27 -0500

    Chapter 8: Configure the ActivitySerializer.

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,2 +1,8 @@
 class ActivitySerializer < ActiveModel::Serializer
+  # look up searchresults_type on the model, but use type in the JSON
+  def type
+    object.searchresults_type
+  end
+
+  attributes :id, :display_id, :type, :display_name, :hotttnesss, :timestamp
 end
